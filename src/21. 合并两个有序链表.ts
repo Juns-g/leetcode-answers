21692cad23cf148325c54092606a9a915fcb1147
Juns <juns.g@foxmail.com>
2024-01-13 21:12:39 +0800

    feat: 21合并链表

---
+++ src/21. 合并两个有序链表.ts
@@ -0,0 +1,52 @@
+// https://leetcode.cn/problems/merge-two-sorted-lists/?envType=study-plan-v2&envId=top-interview-150
+
+class ListNode {
+	val: number;
+	next: ListNode | null;
+	constructor(val?: number, next?: ListNode | null) {
+		this.val = val === undefined ? 0 : val;
+		this.next = next === undefined ? null : next;
+	}
+}
+
+// #region
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+	if (!list1) return list2;
+	if (!list2) return list1;
+
+	const ans = new ListNode();
+	let cur = ans;
+	let i: ListNode | null = list1,
+		j: ListNode | null = list2;
+	while (i && j) {
+		if (i.val > j.val) {
+			cur.next = j;
+			j = j.next;
+		} else {
+			cur.next = i;
+			i = i.next;
+		}
+		cur = cur.next;
+	}
+	cur.next = i ? i : j;
+	return ans.next;
+}
+// #endregion
+
+// #region
+// 递归做法,
+// ! cv到leetcode的时候注意改一下函数名
+function mergeTwoLists2(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+	if (!list1) return list2;
+	if (!list2) return list1;
+	if (list1.val < list2.val) {
+		list1.next = mergeTwoLists2(list1.next, list2);
+		return list1;
+	} else {
+		list2.next = mergeTwoLists2(list2.next, list1);
+		return list2;
+	}
+}
+// #endregion
+
+export default {};
