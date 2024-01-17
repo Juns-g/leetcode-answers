124bc697ca7e41065648d4b139ddf83390cb3101
Juns <juns.g@foxmail.com>
2024-01-17 13:01:58 +0800

    feat: 146(map)

--- src/146. LRU 缓存.ts
+++ src/146. LRU 缓存.ts
@@ -6,13 +6,28 @@ class LRUCache {
 	map: Map<number, number>;
 	constructor(capacity: number) {
 		this.capacity = capacity;
+		this.map = new Map();
 	}
 
 	get(key: number): number {
-		return 0;
+		if (this.map.has(key)) {
+			const val = this.map.get(key) as number;
+			this.map.delete(key);
+			this.map.set(key, val);
+			return val;
+		}
+		return -1;
 	}
 
-	put(key: number, value: number): void {}
+	put(key: number, value: number): void {
+		if (this.map.has(key)) {
+			this.map.delete(key);
+		}
+		this.map.set(key, value);
+		if (this.map.size > this.capacity) {
+			this.map.delete(this.map.keys().next().value);
+		}
+	}
 }
 // #endregion
 
